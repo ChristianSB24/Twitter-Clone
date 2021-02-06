94c6beccec32c1fed50c87df62f3aad1fc28cc9e
ChristianSB24 <christiansaldana10@gmail.com>
2021-02-06 10:13:08 -0600

    Added retweet functionality

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -28,7 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost' '']
 LOGIN_URL = "/login"
+
 MAX_TWEET_LENGTH = 240
+TWEET_ACTION_OPTIONS = ["like", "unlike", "retweet"]
+
 
 
 # Application definition
