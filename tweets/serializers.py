422697521febcf65171e921e4d9b1f6afc4e4b32
ChristianSB24 <christiansaldana10@gmail.com>
2021-02-07 20:13:16 -0600

    Improved Action Btn

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -35,8 +35,6 @@ class TweetCreateSerializer(serializers.ModelSerializer):
 class TweetSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
     parent = TweetCreateSerializer(read_only=True)
-
-
     class Meta:
         model = Tweet
         fields = ['id', 'content', 'likes', 'is_retweet', 'parent']
