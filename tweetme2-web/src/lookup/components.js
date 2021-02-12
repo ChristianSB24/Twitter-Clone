c890efa2880bcc66e2dcdc79896d729d73eb5bf0
ChristianSB24 <christiansaldana10@gmail.com>
2021-02-11 22:48:45 -0600

    Added login redirect

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -1,9 +1,9 @@
 function getCookie(name) {
-  let cookieValue = null;
+  var cookieValue = null;
   if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i].trim();
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
           // Does this cookie string begin with the name we want?
           if (cookie.substring(0, name.length + 1) === (name + '=')) {
               cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
@@ -22,21 +22,26 @@ export function backendLookup(method, endpoint, callback, data) {
   const xhr = new XMLHttpRequest() 
   const url = `http://localhost:8000/api${endpoint}`
   xhr.responseType = "json"
-  const csrftoken = getCookie('token'); // getCookie('csrftoken') in tutorial (Investigate why it didnt work that way.)
+  const csrftoken = getCookie('csrftoken'); // getCookie('csrftoken') in tutorial (Investigate why it didnt work that way.)
   xhr.open(method, url)
   xhr.setRequestHeader("Content-Type", "application/json")
 
   if (csrftoken) {
-    xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
+    // xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
     xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
     xhr.setRequestHeader("X-CSRFToken", csrftoken)
   }
     
   xhr.onload = function() {
+    if (xhr.status === 403 && xhr.response) {
+      const detail = xhr.response.detail
+      if (detail === "Authentication credentials were not provided.") {
+        window.location.href = "/login?showLoginRequired=true"
+      }
+    }
     callback(xhr.response, xhr.status)
   }
   xhr.onerror = function (e) {
-    console.log(e)
     callback({"message": "The request was an error"}, 400)
   }
   xhr.send(jsonData)
