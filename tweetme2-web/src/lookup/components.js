a8daeabcecf7748ddfcca09d7770c9b34282f1e0
ChristianSB24 <christiansaldana10@gmail.com>
2021-02-16 21:30:26 -0600

    Updated url for react app

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -20,7 +20,7 @@ export function backendLookup(method, endpoint, callback, data) {
     jsonData = JSON.stringify(data)
   }
   const xhr = new XMLHttpRequest() 
-  const url = `http://localhost:8000/api${endpoint}`
+  const url = `https://infinite-escarpment-77268.herokuapp.com//api${endpoint}`
   xhr.responseType = "json"
   const csrftoken = getCookie('csrftoken'); // getCookie('csrftoken') in tutorial (Investigate why it didnt work that way.)
   xhr.open(method, url)
