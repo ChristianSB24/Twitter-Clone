8f3bb13bd1090bd610c85f342ee052229c2adfea
ChristianSB24 <christiansaldana10@gmail.com>
2021-02-16 20:44:43 -0600

    Added login, logout, register, edit buttons on navbar

--- tweetme2-web/src/profiles/badge.js
+++ tweetme2-web/src/profiles/badge.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react'
-import numeral from 'numeral'
 
 import {UserDisplay, UserPicture} from './components'
 import {apiProfileDetail, apiProfileFollowToggle} from './lookup'
